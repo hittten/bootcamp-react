4bed6e1b4545ba08eb37dcbc09b6b07bd7d6abd7
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2020-02-17 23:25:18 +0100

    grid view and createAt date

--- src/product-list/ProductList.jsx
+++ src/product-list/ProductList.jsx
@@ -1,37 +1,52 @@
 import React from 'react';
 import './ProductList.css';
 
-function ProductList(props) {
-  const products = props.products.map(product => {
-    return (
-      <li key={product.id}>
-        <h2>{product.name}</h2>
-        <img src={product.image} alt={product.name}/>
-        <p>{product.description.length > 150 ? product.description.slice(0,150) + '...' : product.description}</p>
-        <div className="price">{product.price} €</div>
-        <div>
-          <button className="button">add to cart</button>
-        </div>
-      </li>
-    )
-  });
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="wrapper">
-      <h1>Products</h1>
-      <div className="views">
-        <i className="material-icons">
-          view_list
-        </i>
-        <i className="material-icons">
-          view_module
-        </i>
+    this.state = {
+      gridView: false,
+    }
+  }
+
+  render() {
+    const products = this.props.products.map(product => {
+      return (
+        <li key={product.id}>
+          <h2>{product.name}</h2>
+          <img src={product.image} alt={product.name}/>
+          <p>
+            ({product.createdAt.toLocaleDateString()}) -
+            {product.description.length > 150 ? product.description.slice(0, 150) + '...' : product.description}
+          </p>
+          <div className="price">{product.price} €</div>
+          <div>
+            <button className="button">add to cart</button>
+          </div>
+        </li>
+      )
+    });
+
+    return (
+      <div className="wrapper">
+        <h1>Products</h1>
+        <div className="views">
+          <i className={`material-icons ${!this.state.gridView ? 'selected' : ''}`}
+             onClick={() => this.setState({gridView: false})}>
+            view_list
+          </i>
+          <i className={`material-icons ${this.state.gridView ? 'selected' : ''}`}
+             onClick={() => this.setState({gridView: true})}>
+            view_module
+          </i>
+        </div>
+        <ul className={this.state.gridView ? 'grid' : ''}>
+          {products}
+        </ul>
       </div>
-      <ul>
-        {products}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default ProductList;
